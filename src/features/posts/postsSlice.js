62af163dcc6fd3019b5c3600559fe326edbb59a9
Timothy <timothy.curchod@accenture.com>
2022-08-09 14:24:49 +1000

    created add post form

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = [
+  { id: '1', title: 'First Post!', content: 'Hello!' },
+  { id: '2', title: 'Second Post', content: 'More text' },
+  { id: '3', title: 'Third Post', content: 'Spin test' }
+]
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    postAdded(state, action) {
+      state.push(action.payload)
+    }
+  }
+})
+
+export const { postAdded } = postsSlice.actions
+
+export default postsSlice.reducer
